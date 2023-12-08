bfcf92d7ff9514907379061a04a13519d26800ce
JasonLuis <jason20luis@gmail.com>
2023-12-08 17:47:20 -0300

    chore: resolved typing in storybook files resolved

--- src/core/components/ModalWins/ModalEndGame.stories.ts
+++ src/core/components/ModalWins/ModalEndGame.stories.ts
@@ -1,6 +1,12 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
 import UiModalEndGame from './ModalEndGame.vue';
 
-export default {
+enum Winner {
+  x = 'X',
+  o = 'O'
+}
+
+const meta: Meta<typeof UiModalEndGame> = {
   title: 'Ui/ModalEndGame',
   component: UiModalEndGame,
   argTypes: {
@@ -17,31 +23,33 @@ export default {
   }
 };
 
-const Template = (args: any, { argTypes }: any) => ({
-  props: Object.keys(argTypes),
-  components: { UiModalEndGame },
-  setup() {
-    return { args };
-  },
-  data() {
-    return {};
-  },
-  template: '<ui-modal-end-game v-bind="args">'
-});
+export default meta;
+type Story = StoryObj<typeof UiModalEndGame>;
 
-export const Default = Template.bind({});
-
-Default.args = {
-  modal: true,
-  textWind: 'PLAYER 1 WINS!',
-  winner: 'O'
-};
-Default.parameters = {
-  docs: {
-    source: {
-      code: `<ui-modal-end-game :modal="modal" :textWin="textWind" :winner="winner"/>`,
-      language: 'vue',
-      type: 'auto'
+export const Template: Story = {
+  render: (args, { argTypes }) => ({
+    props: Object.keys(argTypes),
+    components: { UiModalEndGame },
+    setup() {
+      return { args };
+    },
+    data() {
+      return {};
+    },
+    template: '<ui-modal-end-game v-bind="args">'
+  }),
+  args: {
+    modal: true,
+    textWin: 'PLAYER 1 WINS!',
+    winner: 'O' as Winner
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: `<ui-modal-end-game :modal="modal" :textWin="textWind" :winner="winner"/>`,
+        language: 'vue',
+        type: 'auto'
+      }
     }
   }
 };
